de282f2c8a5d6cac94b8612cc50c502808ac0c3d
rossross05 <120494239+rossross05@users.noreply.github.com>
2023-01-25 18:43:07 +0100

    insert the corret function of get forecast

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "9b8bef792df2b76c984d57bd644f6450";
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
